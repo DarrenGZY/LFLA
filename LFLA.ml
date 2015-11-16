617b2c704fcc6f1ade8eb81174ce60abfe04e6ab
DarrenGZY <zyguochn@gmail.com>
2015-11-15 23:04:46 -0500

    make the output python file executable

--- LFLA.ml
+++ LFLA.ml
@@ -1,10 +1,18 @@
 open Printf
 
 let _ =
-    let lexbuf = Lexing.from_channel stdin in 
+    let in_file = if Array.length Sys.argv == 2 then Sys.argv.(1)
+                    else raise (Failure ("usage: ./LFLA [filename]"))
+    in
+    (* let length = String.length in_file in *)
+    let lexbuf = Lexing.from_channel (open_in in_file) in
+    (* let in_file_bytes = Bytes.of_string in_file in *)
+    (* let out_file = (Bytes.sub_string in_file_bytes 0 (length-2)) ^ "py" in *)
     let program = Parser.program Scanner.token lexbuf in
-        let pyFile = open_out "test.py" in
+        let pyFile = open_out "a.out" in
+             fprintf pyFile "%s\n" "#!/usr/local/bin/python";
              fprintf pyFile "%s\n" "import numpy as np";
              fprintf pyFile "%s\n"  (Compile.compile program);
              close_out pyFile;
+             Sys.command ("chmod +x a.out") ;
     
