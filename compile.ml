ff2a08f8c3bebe1b47328225c14417b6a0468aab
DarrenGZY <zyguochn@gmail.com>
2015-11-15 23:06:27 -0500

    compile if, for, while statements, solve the identation problem

--- compile.ml
+++ compile.ml
@@ -1,7 +1,7 @@
 open Ast
 open Parser
 
-
+module StringMap = Map.Make(String)
 let rec string_of_expr = function
     Literal(l) -> l
     | Id(s) -> s
@@ -9,35 +9,40 @@ let rec string_of_expr = function
         string_of_expr e1 ^ " " ^
         (match o with
         Add -> "+" | Sub -> "-" | Mult -> "*" | Div -> "/"
-        | Equal -> "==" | Neq -> "!="
+        | Add_Dot -> "+" | Sub_Dot -> "-" | Mult_Dot -> "*"    
+        | Div_Dot -> "/" | Equal -> "==" | Neq -> "!="
         | Less -> "<" | Leq -> "<=" | Greater -> ">" | Geq -> ">=") ^ " " ^
-        string_of_expr e2
+        string_of_expr e2   (*TODO: missed operators: &&, ||, ', @, <<>>, [[]], etc *)
     | Assign(v, e) -> v ^ " = " ^ string_of_expr e
     | Call(f, el) ->
         f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
     | Noexpr -> ""
 
-
-let rec string_of_stmt = function
+(* num_ident indicates the number of tabs at the begin of the statement, each tab is three spaces *)
+let rec string_of_stmt num_ident = function
     Block(stmts) ->
-        "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
-    | Expr(expr) -> string_of_expr expr ^ "\n";
-    | Return(expr) -> "return " ^ string_of_expr expr ^ "\n";
-    | If(e, s, []) -> "if (" ^ string_of_expr e ^ ")\n" ^ String.concat "" (List.map string_of_stmt s)
-    | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
-        String.concat "" (List.map string_of_stmt s1) ^ "else\n" ^ String.concat "" (List.map string_of_stmt s2)
-    | For(a1, a2, s) ->
-        "for (" ^ string_of_int a1  ^ " ; " ^ string_of_int a2  ^ ") " ^ String.concat "" (List.map string_of_stmt s)
-    | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ String.concat "" (List.map string_of_stmt s)
+        "\n" ^ (String.make (num_ident*3) ' ') ^ String.concat "" (List.map (string_of_stmt (num_ident+1)) stmts) ^ "\n"
+    | Expr(expr) -> (String.make (num_ident*3) ' ') ^ string_of_expr expr ^ "\n";
+    | Return(expr) -> (String.make (num_ident*3) ' ') ^ "return " ^ string_of_expr expr ^ "\n";
+    | If(e, s, []) -> (String.make (num_ident*3) ' ') ^ "if " ^ string_of_expr e ^ ":\n" 
+        ^ String.concat "" (List.map (string_of_stmt (num_ident+1)) s)
+    | If(e, s1, s2) -> (String.make (num_ident*3) ' ') ^ "if " ^ string_of_expr e ^ ":\n"
+        ^ String.concat "" (List.map (string_of_stmt (num_ident+1)) s1) ^ "\n" 
+        ^ (String.make (num_ident*3) ' ') ^ "else:\n" ^ String.concat "" (List.map (string_of_stmt (num_ident+1)) s2) ^ "\n"
+    | For(l, a1, a2, s) ->
+        (String.make (num_ident*3) ' ') ^ "for " ^ l ^ " in range(" ^ a1 ^ ", " ^  a2  ^ ") :\n" 
+        ^ String.concat "" (List.map (string_of_stmt (num_ident+1)) s) ^ "\n"
+    | While(e, s) -> (String.make (num_ident*3) ' ') ^ "while " ^ string_of_expr e ^ ": \n" 
+        ^ String.concat "" (List.map (string_of_stmt (num_ident+1)) s) ^ "\n"
 
 
 let string_of_prim_value = function
     VValue(s) -> s
     | VecValue(s) -> "np.array([" ^ String.concat "," s ^ "])"
     | MatValue(s) -> "np.matrix((" ^ String.concat "," (List.map (fun s -> "(" ^ s ^ ")") (List.map (String.concat ",") s)) ^ "))"
-    | VecSpValue(s) -> String.concat "," s
-    | InSpValue(s1, s2) -> s1 ^ "," ^ s2
-    | AffSpValue(s1, s2) -> s1 ^ "," ^ s2
+    | VecSpValue(s) -> "vecspace(" ^ String.concat "," s ^ ")"      (*TODO: define vecspace in python*)
+    | InSpValue(s1, s2) -> "inspace(" ^ s1 ^ "," ^ s2 ^ ")"         (*TODO: define inspace in python*)
+    | AffSpValue(s1, s2) -> "affspace(" ^ s1 ^ "," ^ s2 ^ ")"       (*TODO: define affspace in python*)
     | Notknown -> ""
 
 
@@ -47,13 +52,20 @@ let string_of_normal_decl = function
 
 let string_of_func_decl fdecl =
     "def " ^ fdecl.fname ^ "() :\n" ^ "   " ^ String.concat "   " (List.map string_of_normal_decl fdecl.locals) ^ 
-    "\n" ^ "   " ^  String.concat "   " (List.map string_of_stmt fdecl.body) ^ "\n"
+    "\n" ^ String.concat "" (List.map (string_of_stmt 1) fdecl.body) ^ "\n"
 
 (* input is ast tree(normal_decl list * func_decl list), output is a python file *)
 let compile (normals, functions) = 
-    String.concat "" (List.map string_of_normal_decl normals) ^
-    String.concat "" (List.map string_of_func_decl functions) ^
-    "main()" 
+    
+    let func_table = 
+        List.fold_left (fun m func -> StringMap.add func.fname 0 m) StringMap.empty functions
+    in
+    if StringMap.mem "main" func_table then
+        String.concat "" (List.map string_of_normal_decl normals) ^
+        String.concat "" (List.map string_of_func_decl functions) ^
+        "main()" 
+    else
+        raise (Failure("no main function"))
     
      
     (*
