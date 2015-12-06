54729add8bd23716051e134c62926c4cf1342d0e
DarrenGZY <zyguochn@gmail.com>
2015-12-06 16:36:28 -0500

    fix some errors

--- VecSpace.py
+++ VecSpace.py
@@ -7,30 +7,35 @@ class VecSpace:
 		vectors: A list of vectors
 	"""
 
-	def __init__(self, vecs):
-		self.vectors = []
+	def __init__(self, vecs=[]):
+		vectors = []
 		
-		# test for all vectors in the vecs list
+		
+                # test for all vectors in the vecs list
 		for i in range(len(vecs)):
-			self.vectors.append(vecs[i])
+			vectors.append(vecs[i])
 			# if # of counts > # of vecs,
 			# the new vector is linear independent with base vectors
-			if not linearIndependent(self.vectors):
-				del self.vectors[-1]
+			if not linearIndependent(vectors):
+				del vectors[-1]
+                self.vectors = vectors
 
 
 	def belongs(self, vec):
-		self.vectors.append(vec)
+	        if self.vectors == []:
+                    return 0
+
+                self.vectors.append(vec)
 		if linearIndependent(self.vectors):
-			print "Belongs to this vectors spaces"
-			return 1
-		else:
-			print "Not belongs to this vectors spaces"
-			del self.vectors[-1]
+			print "Not Belongs to this vectors spaces"
 			return 0
+		else:
+			print "Belongs to this vectors spaces"
+			del self.vectors[-1]
+			return 1
 
 
-	def plus(self, vecspace):
+	def __add__(self, vecspace):
 		temp = self.vectors + vecspace.vectors
 		return VecSpace(temp)
 
